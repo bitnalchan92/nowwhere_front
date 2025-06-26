e658751b9f3376454094b00134a2f0a03701fc7b
bitnalchan92 <bitnalchan92@gmail.com>
2025-06-26 16:32:04 +0900

    env update

--- lib/busApi.ts
+++ lib/busApi.ts
@@ -1,9 +1,9 @@
 import axiosInstance from "@/lib/axiosInstance";
-import {NearBusStop} from "@/types/transit";
-export const getNearBusStops = async (): Promise<NearBusStop[]> => {
+import { NearBusStop } from "@/types/transit";
 
+export const getNearBusStops = async (): Promise<NearBusStop[]> => {
   try {
-    const response = await axiosInstance.get(`/api/bus/nearBusStops`)
+    const response = await axiosInstance.get(`/api/bus/nearBusStops`);
     console.log(response);
 
     return response.data;
@@ -11,4 +11,4 @@ export const getNearBusStops = async (): Promise<NearBusStop[]> => {
     console.error("근접 버스 정류장 조회 실패 : ", error);
     return [];
   }
-}
+};
